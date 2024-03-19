2247bb83ae008e737af0696afb3ac772d6b8da8a
ankanbasu2 <163997912+ankanbasu2@users.noreply.github.com>
2024-03-19 16:38:16 +0530

    Add files via upload

---
+++ Day 02/first_CSharp_app/first_CSharp_app/Program.cs
@@ -0,0 +1,123 @@
+﻿// See https://aka.ms/new-console-template for more informat
+
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+#region variables
+bool isPermanent = true;
+string name = "Naruto";
+int vehicles = 2;
+string village = "Konoha";
+
+Console.WriteLine("My name is " + name);
+Console.WriteLine("I have " + vehicles + " vehicles");
+Console.WriteLine("I live in: " + village);
+Console.WriteLine("Am I a permanent employee: " + isPermanent);
+#endregion
+
+#region Calculation
+//#region var intialisation
+//int firstnumber, secondnumber, thirdnumber;
+//int multiplication, division, addition, subtraction;
+//#endregion
+
+//#region userinput
+//console.writeline("pls enter first number:");
+//firstnumber = convert.toint32(console.readline());
+
+//console.writeline("pls enter second number:");
+//secondnumber = convert.toint32(console.readline());
+//#endregion
+
+//#region calc
+//addition = firstnumber + secondnumber;
+//subtraction = firstnumber - secondnumber;
+//multiplication = firstnumber * secondnumber;
+//division = firstnumber / secondnumber;
+//#endregion
+
+//#region output
+//console.writeline("addition: " + addition);
+//console.writeline("subtraction: " + subtraction);
+//console.writeline("multiplication: " + multiplication);
+//console.writeline("division: " + division);
+//#endregion
+#endregion
+
+#region task1
+//int number;
+
+//Console.Write("Pls enter a number: ");
+//number = Convert.ToInt32(Console.ReadLine());
+
+//switch(number)
+//{
+//    case int i when i >= 1 && i <= 10:
+//        Console.WriteLine("Poor number.");
+//        break;
+//    case int i when i > 10 && i <= 25:
+//        Console.WriteLine("Average number.");
+//        break;
+//    case int i when i > 25 && i <=50:
+//        Console.WriteLine("Good number.");
+//        break;
+//    case int i when i > 50 && i <= 75:
+//        Console.WriteLine("Very good marks");
+//        break;
+//    case int i when i > 75 && i <= 100:
+//        Console.WriteLine("Excellent number");
+//        break;
+//    default: 
+//        Console.WriteLine("Invalid number");
+//        break;
+
+//}
+#endregion
+
+#region loop
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(i + ": Hello, World");
+//}
+//int num_for_table;
+
+//Console.Write("Enter number to print multiplication table: ");
+//num_for_table = Convert.ToInt32(Console.ReadLine());
+
+//for (int i=1; i<=10; i++)
+//{
+//    Console.WriteLine(num_for_table + " * " +  i + " = " + num_for_table*i);
+//}
+#endregion
+
+#region loop2
+//int userInps = 20, evens = 0, odds = 0, negs = 0, tot = 0, num;
+
+//for(int i=0; i<userInps; i++)
+//{
+//    Console.Write("Entger number: ");
+//    num = Convert.ToInt32(Console.ReadLine());
+
+//    tot += num;
+
+
+//    if (num%2 == 0)
+//    {
+//        evens++;
+//    } else
+//    {
+//        odds++;
+//    }
+//    if (num <0)
+//    {
+//        negs++;
+//    }
+//}
+
+//Console.WriteLine("----------Summary----------");
+//Console.WriteLine("Total: " + tot);
+//Console.WriteLine("Evens: " + evens);
+//Console.WriteLine("Odds: " + odds);
+//Console.WriteLine("Negetives: " + negs);
+#endregion
