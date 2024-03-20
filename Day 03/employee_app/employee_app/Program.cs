7e08acc8cca787fd89b00ed835a74481122368a7
ankanbasu2 <163997912+ankanbasu2@users.noreply.github.com>
2024-03-20 15:48:41 +0530

    Add files via upload

---
+++ Day 03/employee_app/employee_app/Program.cs
@@ -0,0 +1,186 @@
+﻿// See https://aka.ms/new-console-template for more information
+string choice, choice2, uname, passwd;
+bool flg1, flg2;
+
+Console.WriteLine("**********Welcome to Employee Management System**********");
+Console.Write("Press ENTER to login: ");
+Console.ReadLine();
+flg1 = true;
+
+while(flg1)
+{
+Console.WriteLine("1. Employee");
+Console.WriteLine("2. Admin");
+Console.WriteLine("3. Exit");
+
+Console.Write("Enter choice: ");
+choice = Console.ReadLine();
+
+switch(choice)
+{
+    case "1":
+        #region emp_case
+        Console.WriteLine("Welcome Employee.\n");
+
+        Console.Write("Enter Username: ");
+        uname = Console.ReadLine();
+        if (uname == "ANKAN")
+        {
+            Console.Write("Enter password: ");
+            passwd = Console.ReadLine();
+
+            if (passwd == "ankan@123")
+            {
+                Console.WriteLine("Login Successful, Welcome " + uname);
+                flg2 = true;
+
+                #region emp_submenu
+                while (flg2)
+                { 
+                    Console.WriteLine("\n1. Check Employee Details.");
+                    Console.WriteLine("2. Apply Leave");
+                    Console.WriteLine("3. Raise a request to manager");
+                    Console.WriteLine("4. Submit a Bill");
+                    Console.WriteLine("5. Fill Daily Worksheet");
+                    Console.WriteLine("6. Exit\n");
+
+                    Console.Write("Enter choice: ");
+                    choice2 = Console.ReadLine();
+
+                    switch(choice2)
+                    {
+                        case "1":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break; 
+                        case "2":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine(); 
+                            break; 
+                        case "3":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break; 
+                        case "4":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "5":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "6":
+                            flg2 = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input");
+                            break;
+                    }
+                }
+                #endregion
+            }
+            else
+            {
+                Console.WriteLine("Invalid Password");
+            }
+        } else
+        {
+            Console.WriteLine("Invalid Username.");
+        }
+        #endregion
+        break;
+    case "2":
+        #region admin_case
+        Console.WriteLine("Welcome Admin.");
+        Console.Write("Enter Username: ");
+        uname = Console.ReadLine();
+        if (uname == "Admin")
+        {
+            Console.Write("Enter password: ");
+            passwd = Console.ReadLine();
+
+            if (passwd == "E&Y@987654321")
+            {
+                Console.WriteLine("Login Successful, Welcome " + uname);
+                flg2 = true;
+
+                #region admin_submenu
+                while (flg2)
+                {
+                    Console.WriteLine("\n1. Add new Employee");
+                    Console.WriteLine("2. View Employee Details");
+                    Console.WriteLine("3. Delete Employee");
+                    Console.WriteLine("4. Approve Leaves");
+                    Console.WriteLine("5. Approve voucher");
+                    Console.WriteLine("6. Announcement");
+                    Console.WriteLine("7. Exit\n");
+
+                    Console.Write("Enter choice: ");
+                    choice2 = Console.ReadLine();
+
+                    switch (choice2)
+                    {
+                        case "1":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "2":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "3":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "4":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "5":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "6":
+                            Console.WriteLine("Option " + choice2 + " selected");
+                            Console.Write("Press Enter to return.");
+                            Console.ReadLine();
+                            break;
+                        case "7":
+                            flg2 = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input");
+                            break;
+                    }
+                }
+                #endregion
+            }
+            else
+            {
+                Console.WriteLine("Invalid Password");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Username.");
+        }
+#endregion
+        break;
+    case "3":
+        flg1 = false;
+        break;
+    default: 
+        Console.WriteLine("Invalid Input");
+        break;
+}
+}
---
+++ Day 03/file_io/file_io/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.IO;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//FileStream myFile = new FileStream("info.txt", FileMode.CreateNew, FileAccess.Write);
+//StreamWriter myPen = new StreamWriter(myFile);
+
+//myPen.WriteLine("Hello and welcome to my intro.");
+//myPen.WriteLine("My name is Naruto.");
+//myPen.WriteLine("I live in Konoha Village.");
+//myPen.WriteLine("I am Hokage.");
+//myPen.WriteLine("Thank You.");
+
+//myPen.Close();
+//myFile.Close();
+
+#region file_write2
+//string name, city, movie, food, sports;
+
+//Console.Write("Enter you name: ");
+//name = Console.ReadLine();
+
+//Console.WriteLine("Welcome, " + name + "\nPls enter the following details.");
+
+//FileStream myFile = new FileStream(name+"_info.txt", FileMode.Create, FileAccess.Write);
+//StreamWriter myWriter = new StreamWriter(myFile);
+
+//myWriter.WriteLine("Name: " + name);
+
+//Console.WriteLine("Pls enter your city: ");
+//city = Console.ReadLine();
+//myWriter.WriteLine("City: " + city);
+
+//Console.WriteLine("Pls enter your favourite movie: ");
+//movie = Console.ReadLine();
+//myWriter.WriteLine("Movie: " + movie);
+
+//Console.WriteLine("Pls enter your favourite food: ");
+//food = Console.ReadLine();
+//myWriter.WriteLine("Food: " + food);
+
+
+//Console.WriteLine("Pls enter your favourite sports: ");
+//sports = Console.ReadLine();
+//myWriter.WriteLine("Sports: " + sports);
+
+//myWriter.Close();
+//myFile.Close();
+
+//Console.WriteLine("Thank you, " + name + ", for the details.");
+#endregion
+
+#region file_read
+//Console.Write("Welcome to EMS.\nPress ENTER to Login:");
+
+//Console.Write("Enter uname: ");
+//unameEntered = Console.ReadLine();
+
+//Console.Write("Enter password: ");
+//passwdEntered = Console.ReadLine();
+
+//if (unameEntered == null || passwdEntered == null)
+//{
+
+//}
+#endregion
+
+#region file_read_write
+string fileName = "autonumber.txt";
+int val;
+
+if (File.Exists(fileName))
+{
+    // read no. increment. write back.
+    FileStream myFile = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+    
+    StreamReader reader = new StreamReader(myFile);
+    val = Convert.ToInt32(reader.ReadLine());
+    reader.Close();
+    Console.WriteLine($"Value read: {val}");
+   
+    val++;
+
+    StreamWriter writer = new StreamWriter(fileName);
+    writer.WriteLine(val);
+    writer.Close();
+    Console.WriteLine($"Value written: {val}");
+
+    myFile.Close();
+} else
+{
+    // create file. write default val 1
+    FileStream myFile = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+
+    StreamWriter writer = new StreamWriter(fileName);
+    writer.WriteLine("1");
+    writer.Close();
+
+    myFile.Close();
+
+    Console.WriteLine($"File \'{fileName}\' created with default value \'1\'");
+}
+#endregion
